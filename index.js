0142f1d6d54a883948ebcb4d31da5219486af991
cannila <boynakcamila@gmail.com>
2022-04-09 20:51:39 -0300

    1 entrega del desafio final y de la actividad de la clase 4

--- index.js
+++ index.js
@@ -45,6 +45,11 @@ console.log(cabana6.capacidadMaxima);
 console.log(cabana6.precio);
 */
 
+
+
+
+// FUNCIONES EN CABAÑAS
+
 function Cabana(tipoDeCabana, capacidadMaxima, precio) {
     this.tipoDeCabana = tipoDeCabana;
     this.capacidadMaxima = capacidadMaxima;
@@ -65,3 +70,108 @@ console.log(cabana3.precio);
 console.log(cabana6.tipoDeCabana);
 console.log(cabana6.capacidadMaxima);
 console.log(cabana6.precio);
+
+
+//  P R I M E R A    E N T R E G A   D E L   P R O Y E C T O  F I N A L 
+
+class Cabana {
+    constructor(nombre, precio, capacidadMaxima) {
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+        this.reservada = false;
+        this.capacidadMaxima = "";
+
+    }
+    sumaIva () {
+        this.precio = this.precio * 1,21;
+    }
+}
+
+//agrego arrays- las cabanas- ESTA BIEN ASI?
+
+const cabana = [];
+cabana.push (new cabana ("Cabaña para dos personas", 1000, 2));
+cabana.push (new cabana ("Cabaña para tres personas", 1500, 3));
+cabana.push (new cabana ("Cabaña familiar", 3000, 6));
+cabana.push (new cabana ("Cabaña para solo travellers", 700, 8));
+
+//for ... of para modificar todos los precios asi se muestran con el iva
+
+for (const cabana of cabanas) {
+    cabana.sumaIva (); //que pongo en los ()?
+}
+
+
+
+
+const cabanas = [
+    {nombre: "cabanaDos", precio: 1000, id: 1},
+    {nombre: "cabanaTres", precio: 1500, id: 2},
+    {nombre: "cabanaFamilia", precio:3000, id: 3},
+]
+cabanas.push({nombre: "cabanaSolo", precio: 700, id: 4})
+
+console.log(cabanas);
+
+// A C T I V I D A D E S 
+
+const actividades = [
+    {nombre: "actividadPaseo", precio: 1000, id: 1, duracion: "75 minutos", exterior: true},
+    {nombre: "actividadKayak", precio: 1500, id: 2, duracion: "90 minutos", exterior: true},
+    {nombre: "actividadLancha", precio: 3000, id: 3, duracion: "2 horas", exterior: true},
+    {nombre: "actividadTennis", precio: 2500, id: 4, duracion: "60 minutos", exterior: true},
+    {nombre: "actividadCocinaVeggie", precio: 2500, id: 5, duracion: "2 horas", exterior: false},
+]
+
+cabanas.push({nombre: "actividadCrochet", precio: 2500, id: 6, duracion: "2 horas", exterior: false})
+cabanas.push({nombre: "actividadCeramica", precio: 3000, id: 7, duracion: "2 horas", exterior: false})
+
+console.log(actividades);
+
+
+
+for (const actividadTennis of actividades) {
+    console.log(actividadTennis.duracion);
+    console.log(actividadTennis.exterior);
+}
+
+
+
+
+/*
+class Cabanas {
+    constructor(id, )
+}
+
+let baseDatos = [];
+
+function crearReserva() {
+    let reserva = new Reserva(
+        prompt("Ingrese las fechas de su reserva"),
+        prompt("Ingrese el tipo de cabaña"),
+        prompt("Ingrese la cantidad de  huéspedes"),
+        prompt("Ingrese su pedido especial aquí:")
+    );
+    baseDatos.push(reserva);
+}
+*/
+
+
+
+function modificarReserva(params) {
+    if (reserva.length == 0) {
+        alert("No hay ninguna reserva hecha");
+    } else {
+        let modificar = prompt("Ingrese el código de la reserva que quiere modificar");
+        for (const producto of baseDatos) {
+            if (reserva.id === parseInt (modificar)) {
+                reserva.id = parseInt(prompt("Ingrese el nuevo número de reserva"));
+                reserva
+
+                
+            }
+            
+        }
+
+    }
+}
